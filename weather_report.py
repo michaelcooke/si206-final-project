970e6e6d2ae811a6678691d34e4d6662f926d96c
Michael Cooke <michael.frank.cooke@gmail.com>
2020-12-07 17:16:59 -0500

    🤪 Rewrite to implement required bad practices! 🤪

--- weather_report.py
+++ weather_report.py
@@ -1,191 +1,175 @@
 import aiohttp
 import asyncio
+import json
 import os
-import re
 import sqlite3
 
-def is_k_space_system(system_name, cur, conn):
-  if system_name not in get_system_names(cur, conn) or re.match(r'J\d{6}', system_name) or re.match(r'AD\d{3}', system_name) or re.match(r'P-\d{3}', system_name) or system_name == 'Thera':
-    return False
-  return True
-
-def get_k_space_system_ids(cur, conn):
-  k_space_system_ids = []
-
-  results = cur.execute('SELECT id FROM systems').fetchall()
-  for i in range(len(results)):
-    if is_k_space_system(results[i][0], cur, conn):
-      k_space_system_ids.append(results[i][0])
-
-  return k_space_system_ids
-
-def get_region_ids(cur, conn):
-  results = cur.execute('SELECT id FROM regions').fetchall()
-  return [results[i][0] for i in range(len(results))]
-
-def get_constellation_ids(cur, conn):
-  results = cur.execute('SELECT id FROM constellations').fetchall()
-  return [results[i][0] for i in range(len(results))]
-
-def get_system_names(cur, conn):
-  results = cur.execute('SELECT name FROM systems').fetchall()
-  return [results[i][0] for i in range(len(results))]
-
-def get_system_ids(cur, conn):
-  results = cur.execute('SELECT id FROM systems').fetchall()
-  return [results[i][0] for i in range(len(results))]
-
-def get_system_name(system_id, cur, conn):
-  return cur.execute('SELECT name FROM systems WHERE id = ?', [system_id]).fetchall()[0][0]
-
-async def get_system_jumps(session):
-  url = 'https://esi.evetech.net/latest/universe/system_jumps?datasource=tranquility&language=en-us'
-  try:
-    async with session.get(url) as response:
-      if response.status == 200:
-        return await response.json(content_type='application/json')
-      return await get_system_jumps(session)
-  except:
-    return await get_system_jumps(session)
-
 async def get_killmail(session, killmail_id, killmail_hash):
-  print('Getting killmail id/hash: ' + str(killmail_id) + '/' + str(killmail_hash))
-  url = 'https://esi.evetech.net/latest/killmails/' + str(killmail_id) + '/' + str(killmail_hash) + '?datasource=tranquility&language=en-us'
   try:
-    async with session.get(url) as response:
-      if response.status == 200:
-        return await response.json(content_type='application/json')
-      return await get_killmail(session, killmail_id, killmail_hash)
+    async with session.get('https://esi.evetech.net/latest/killmails/' + str(killmail_id) + '/' + str(killmail_hash) + '?datasource=tranquility&language=en-us') as response:
+      return await response.json(content_type='application/json')
   except:
     return await get_killmail(session, killmail_id, killmail_hash)
 
-async def get_region_kills_past_seconds_ago(session, region_id, seconds):
-  print('Getting kills from region id: ' + str(region_id))
-  url = 'https://zkillboard.com/api/regionID/' + str(region_id) + '/pastSeconds/' + str(seconds) + '/'
+async def get_zkill_regional_past_hour_kills(session, region_id):
+  url = 'https://zkillboard.com/api/regionID/' + str(region_id) + '/pastSeconds/' + str(3600) + '/'
   try:
     async with session.get(url) as response:
       return await response.json(content_type='application/json')
   except:
-    return await get_region_kills_past_seconds_ago(session, region_id, seconds)
+    return await get_zkill_regional_past_hour_kills(session, region_id)
 
-async def seed_killmails_and_jumps(session, seconds, cur, conn):
-  print('Dropping old killmails and jump information')
-  cur.execute('DELETE FROM killmails')
-  cur.execute('DELETE FROM jumps')
-  conn.commit()
+async def get_system_jumps(session):
+  try:
+    async with session.get('https://esi.evetech.net/latest/universe/system_jumps?datasource=tranquility&language=en-us') as response:
+      return await response.json(content_type='application/json')
+  except:
+    return await get_system_jumps(session)
 
-  print('Seeding killmails from past ' + str(seconds) + ' seconds')
+async def get_regions(session):
+  try:
+    async with session.get('https://esi.evetech.net/latest/universe/regions/?datasource=tranquility&language=en-us') as response:
+      return await response.json(content_type='application/json')
+  except:
+    return await get_regions(session)
 
-  zkill_killmails = []
-  for region_id in get_region_ids(cur, conn):
-    for killmail in await get_region_kills_past_seconds_ago(session, region_id, seconds):
-      zkill_killmails.append(killmail)
+async def get_region(session, region_id):
+  try:
+    async with session.get('https://esi.evetech.net/latest/universe/regions/' + str(region_id) + '/?datasource=tranquility&language=en-us') as response:
+      return await response.json(content_type='application/json')
+  except:
+    return await get_region(session, region_id)
 
-  killmails = await asyncio.gather(*[get_killmail(session, zkill_killmail['killmail_id'], zkill_killmail['zkb']['hash']) for zkill_killmail in zkill_killmails])
+async def get_systems(session):
+  try:
+    async with session.get('https://esi.evetech.net/latest/universe/systems/?datasource=tranquility&language=en-us') as response:
+      return await response.json(content_type='application/json')
+  except:
+    return await get_systems(session)
 
-  for entry in await get_system_jumps(session):
-    cur.execute('INSERT INTO jumps (id, jumps) VALUES (?, ?)', (
-      entry['system_id'],
-      entry['ship_jumps']
-    ))
-    conn.commit()
+async def get_system(session, system_id):
+  try:
+    async with session.get('https://esi.evetech.net/latest/universe/systems/' + str(system_id) + '/?datasource=tranquility&language=en-us') as response:
+      return await response.json(content_type='application/json')
+  except:
+    return await get_system(session, system_id)
 
-  for killmail in killmails:
-    cur.execute('INSERT INTO killmails (id, system_id, war_kill, killmail_time) VALUES (?, ?, ?, ?)', (
-      killmail['killmail_id'],
-      killmail['solar_system_id'],
-      True if 'war_id' in killmail else False,
-      killmail['killmail_time']
-    ))
-    conn.commit()
+async def get_data(session):
+  if not os.path.exists('systems.json'):
+    systems = await asyncio.gather(*[get_system(session, system_id) for system_id in await get_systems(session)])
+    with open('systems.json', 'w') as file:
+      json.dump(systems, file)
+      file.close()
+  if not os.path.exists('jumps.json'):
+    jumps = await get_system_jumps(session)
+    with open('jumps.json', 'w') as file:
+      json.dump(jumps, file)
+      file.close()
+  if not os.path.exists('killmails.json'):
+    zkill_killmails = []
+    for region_id in [region for region in await get_regions(session)]:
+      for killmail in await get_zkill_regional_past_hour_kills(session, region_id):
+        zkill_killmails.append(killmail)
+    killmails = await asyncio.gather(*[get_killmail(session, zkill_killmail['killmail_id'], zkill_killmail['zkb']['hash']) for zkill_killmail in zkill_killmails])
+    with open('killmails.json', 'w') as file:
+      json.dump(killmails, file)
+      file.close()
 
-def kills_donut_graph(cur, conn):
-  killmails = cur.execute('SELECT killmails.id, killmails.system_id, systems.name, systems.security_status FROM killmails INNER JOIN systems ON killmails.system_id=systems.id').fetchall()
-  
-  highsec_kills = 0
-  lowsec_kills = 0
-  nullsec_kills = 0
-  kills_1_0 = 0
-  kills_0_9 = 0
-  kills_0_8 = 0
-  kills_0_7 = 0
-  kills_0_6 = 0
-  kills_0_5 = 0
-  kills_0_4 = 0
-  kills_0_3 = 0
-  kills_0_2 = 0
-  kills_0_1 = 0
-  kills_n_0_0 = 0
-  kills_n_0_1 = 0
-  kills_n_0_2 = 0
-  kills_n_0_3 = 0
-  kills_n_0_4 = 0
-  kills_n_0_5 = 0
-  kills_n_0_6 = 0
-  kills_n_0_7 = 0
-  kills_n_0_8 = 0
-  kills_n_0_9 = 0
-  kills_n_1_0 = 0
-
-  for killmail in killmails:
-    if killmail[3] >= 0.5 and killmail[3] <= 1.0:
-      highsec_kills += 1
-      if killmail[3] == 1.0:
-        kills_1_0 += 1
-      elif killmail[3] == 0.9:
-        kills_0_9 += 1
-      elif killmail[3] == 0.8:
-        kills_0_8 += 1
-      elif killmail[3] == 0.7:
-        kills_0_7 += 1
-      elif killmail[3] == 0.6:
-        kills_0_6 += 1
-      elif killmail[3] == 0.5:
-        kills_0_5 += 1
-    elif killmail[3] >= 0.1 and killmail[3] <= 0.4:
-      lowsec_kills += 1
-      if killmail[3] == 0.4:
-        kills_0_4 += 1
-      elif killmail[3] == 0.3:
-        kills_0_3 += 1
-      elif killmail[3] == 0.2:
-        kills_0_2 += 1
-      elif killmail[3] == 0.1:
-        kills_0_1 += 1
+def store_data(cur, conn, silly_asinine_arbitrary_row_limit):
+  with open('systems.json', 'r') as systems_file, open('jumps.json', 'r') as jumps_file, open('killmails.json', 'r') as killmails_file, open('executions.txt', 'a+') as executions_file:
+    systems = json.load(systems_file)
+    systems_file.close()
+    jumps = json.load(jumps_file)
+    jumps_file.close()
+    killmails = json.load(killmails_file)
+    killmails_file.close()
+    current_silly_asinine_arbitrary_row = 0
+    executions_file.seek(0)
+    if executions_file.read() == '':
+      executions = 0
     else:
-      nullsec_kills += 1
-      if killmail[3] == -1.0:
-        kills_n_1_0 += 1
-      elif killmail[3] == -0.9:
-        kills_n_0_9 += 1
-      elif killmail[3] == -0.8:
-        kills_n_0_8 += 1
-      elif killmail[3] == -0.7:
-        kills_n_0_7 += 1
-      elif killmail[3] == -0.6:
-        kills_n_0_6 += 1
-      elif killmail[3] == -0.5:
-        kills_n_0_5 += 1
-      elif killmail[3] == -0.4:
-        kills_n_0_4 += 1
-      elif killmail[3] == -0.3:
-        kills_n_0_3 += 1
-      elif killmail[3] == -0.2:
-        kills_n_0_2 += 1
-      elif killmail[3] == -0.1:
-        kills_n_0_1 += 1
-      elif killmail[3] == 0.0:
-        kills_n_0_0 += 1
+      executions_file.seek(0)
+      executions = int(executions_file.read()[-1])
+    executions_file.seek(0)
+    if executions == 0:
+      cur.execute('CREATE TABLE IF NOT EXISTS systems (id INTEGER PRIMARY KEY, name VARCHAR(255), security_status REAL)')
+      cur.execute('CREATE TABLE IF NOT EXISTS jumps (id INTEGER PRIMARY KEY, jumps INTEGER)')
+      cur.execute('CREATE TABLE IF NOT EXISTS killmails (id INTEGER PRIMARY KEY, system_id INTEGER, war_kill BOOLEAN, killmail_time DATETIME)')
+    for system in systems:
+      if len(cur.execute('SELECT * FROM systems WHERE id == ' + str(system['system_id'])).fetchall()) == 0:
+        cur.execute('INSERT INTO systems (id, name, security_status) VALUES (?, ?, ?)', (
+          system['system_id'],
+          system['name'],
+          round(float(system['security_status']), 1)
+        ))
+        conn.commit()
+        current_silly_asinine_arbitrary_row += 1
+        if executions < 4 and current_silly_asinine_arbitrary_row == silly_asinine_arbitrary_row_limit:
+          executions_file.seek(0)
+          if executions_file.read() == '':
+            executions = 0
+          else:
+            executions_file.seek(0)
+            executions = int(executions_file.read()[-1])
+          executions_file.seek(0)
+          executions_file.write(str(executions+1))
+          executions_file.truncate()
+          executions_file.close()
+          return None
+    for jump in jumps:
+      if len(cur.execute('SELECT * FROM jumps WHERE id == ' + str(jump['system_id'])).fetchall()) == 0:
+        cur.execute('INSERT INTO jumps (id, jumps) VALUES (?, ?)', (
+          jump['system_id'],
+          jump['ship_jumps']
+        ))
+        conn.commit()
+        current_silly_asinine_arbitrary_row += 1
+        if executions < 4 and current_silly_asinine_arbitrary_row == silly_asinine_arbitrary_row_limit:
+          executions_file.seek(0)
+          if executions_file.read() == '':
+            executions = 0
+          else:
+            executions_file.seek(0)
+            executions = int(executions_file.read()[-1])
+          executions_file.seek(0)
+          executions_file.write(str(executions+1))
+          executions_file.truncate()
+          executions_file.close()
+          return None
+    for killmail in killmails:
+      if len(cur.execute('SELECT * FROM killmails WHERE id == ' + str(killmail['killmail_id'])).fetchall()) == 0:
+        cur.execute('INSERT INTO killmails (id, system_id, war_kill, killmail_time) VALUES (?, ?, ?, ?)', (
+          killmail['killmail_id'],
+          killmail['solar_system_id'],
+          True if 'war_id' in killmail else False,
+          killmail['killmail_time']
+        ))
+        conn.commit()
+        current_silly_asinine_arbitrary_row += 1
+        if executions < 4 and current_silly_asinine_arbitrary_row == silly_asinine_arbitrary_row_limit:
+          executions_file.seek(0)
+          if executions_file.read() == '':
+            executions = 0
+          else:
+            executions_file.seek(0)
+            executions = int(executions_file.read()[-1])
+          executions_file.seek(0)
+          executions_file.write(str(executions+1))
+          executions_file.truncate()
+          executions_file.close()
+          return None
+
+def process_data(cur, conn):
+  pass
 
 async def main(loop):
   connector = aiohttp.TCPConnector(limit=50)
   async with aiohttp.ClientSession(loop=loop, connector=connector) as session:
-    name = 'database.db'
-    path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path + '/' + name)
-    cur  = conn.cursor()
-    await seed_killmails_and_jumps(session, 60*60, cur, conn)
-    kills_donut_graph(cur, conn)
+    await get_data(session)
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    store_data(cur, conn, silly_asinine_arbitrary_row_limit = 25)
+    process_data(cur, conn)
 
 if __name__ == '__main__':
   loop = asyncio.get_event_loop()
